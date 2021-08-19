5f58a439820f86de5560a01210a1d080d52ba672
Chandra Anil <56503386+chandra270@users.noreply.github.com>
2021-08-19 15:27:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>TS FOUNDATION BANK</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div class="row intro py-1" style="background-color : #7d5b3e;">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center" style="margin-top: 9rem; margin-bottom: 3rem;">
+                  <h3>WELCOME TO </h3>
+                  <h1>TS FOUNDATION BANK</h1>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md img text-center">
+
+                <img src="image/bankpic.png" class="img-fluid pt-2">
+              </div>
+            </div>
+
+      <!-- Activity section -->
+
+            <div class="row activity text-center" style="background-color : #91644c;">
+                  <div class="col-md act">
+                    <img src="image/view.png" width="100" height="100" class="img-fluid">
+                    <br>
+                    <a href="viewuser.php"><button style="background-color :  #332924;">View Users</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="image/transfer.png" width="120" height="100" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button style="background-color : #332924;">Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="image/transactionhistory1.png" width="100" height="100" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><button style="background-color : #332924;">Transaction History</button></a>
+                  </div>
+            </div>
+      </div>
+      <footer class="text-center mt-5 py-2" style="background-color: #ffffff">
+        <p>&copy 2021.<b> Chandra Anil</b> <br> The Sparks Foundation 2021</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar -->
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="https://www.thesparksfoundationsingapore.org/"> <img src="image/SPARK_LOGO.png", height=70px > </a>
+      <a class="navbar-brand" href="index.php" style="color : #f7dbcb;"><b>THE SPARKS FOUNDATION </b></a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #f7dbcb;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="viewuser.php" style="color : #f7dbcb;"><b>View Customer</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #f7dbcb;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #f7dbcb;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
